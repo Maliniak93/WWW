9a92de97f9064dee01db29f0b732e45c418eb822
unknown <mazepamalina@gmail.com>
2020-10-05 17:03:14 +0200

    udoskonalanie responsywnośći strony

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ header.classList.toggle('sticky', window.scrollY>0);
 })
 function toggleMenu(){
     var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
     menuToggle.classList.toggle('active')
+    menu.classList.toggle('active')
 }
